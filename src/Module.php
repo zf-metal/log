5da0c86a5e4d5bb9ac96cd71c6ea0806abd9e8c0
crist <cristian.cdi@gmail.com>
2018-02-25 15:50:52 -0300

    First Version

--- src/Module.php
+++ src/Module.php
@@ -4,8 +4,6 @@ namespace ZfMetal\Log;
 
 class Module {
 
-    const VERSION = '3.0.2dev';
-
     public function getConfig() {
         return include __DIR__ . '/../config/module.config.php';
     }
